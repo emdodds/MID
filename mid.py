fixed gradient

mid.py
@@ -6,20 +6,20 @@ Created on Tue Feb 16 16:52:02 2016
 """
 
 import numpy as np
-import strfanalysis
+#import strfanalysis
 from scipy.optimize import minimize
 
 class MID:
     """A class to find the maximally informative stimulus dimensions for given stimuli and spikes. 
     Currently only finds the single most informative dimension."""
     
-    def __init__(self, analyzer=None, nbins=15):
-        """Input: analyzer, an object with a generator() method that returns an iterator
-        over stim, spikecount pairs. analyzer also needs a strfshape attribute."""
-        if analyzer is None:
-            self.analyzer = strfanalysis.STRFAnalyzer()
+    def __init__(self, handler=None, nbins=15):
+        """Input: handler, an object with a generator() method that returns an iterator
+        over stim, spikecount pairs. handler also needs a stimshape attribute."""
+        if handler is None:
+            self.handler = strfanalysis.STRFAnalyzer()
         else:
-            self.analyzer = analyzer
+            self.handler = handler
             
         self.v = self.vec_init('sta')
         self.nbins = nbins
@@ -27,13 +27,17 @@ class MID:
         
     def vec_init(self, method='random'):
         """If random, return a random normalized vector. Otherwise return a random normalized stimulus."""
-        if method =='random':
-            length = np.prod(self.analyzer.strfshape)
-            vec = np.random.randn(length)
-        elif method=='sta':
-            vec = self.analyzer.get_STA() - self.analyzer.get_stimave()
-        else:
-            vec = self.analyzer.rand_stimresp(1)[0][0]
+        try:
+            if method =='random':
+                length = np.prod(self.handler.stimshape)
+                vec = np.random.randn(length)
+            elif method=='sta':
+                vec = self.handler.get_STA() - self.handler.get_stimave()
+            else:
+                vec = self.handler.rand_stimresp(1)[0][0]
+        except AttributeError:
+            print('Specialized initialization failed. Falling back on first stimulus.')
+            vec = self.handler.generator().__next__()[0]
         vec = vec/np.linalg.norm(vec)
         return vec
             
@@ -42,11 +46,11 @@ class MID:
             nbins=self.nbins
         if edgefrac is None:
             edgefrac = 1/(5*nbins)
-#        stims = self.analyzer.rand_stimresp(1000)[0]
+#        stims = self.handler.rand_stimresp(1000)[0]
 #        projections = stims.dot(self.v)
-        projections = np.zeros(self.analyzer.get_nstim())
+        projections = np.zeros(self.handler.get_nstim())
         ii=0
-        for stim,_ in self.analyzer.generator():
+        for stim,_ in self.handler.generator():
             projections[ii]=self.v.dot(stim)
             ii+=1
         projections = np.sort(projections)
@@ -68,9 +72,11 @@ class MID:
         """Returns the mutual information between spike arrival times and the projection along v."""
         if v is None:
             v = self.v
+        self.decide_bins() 
+        
         pv = np.zeros(self.nbins) # prob dist of projections
         pvt = np.zeros_like(pv) # prob dist of projections given spike (t for trigger)
-        for stim, sp in self.analyzer.generator():
+        for stim, sp in self.handler.generator():
             proj = self.v.dot(stim)
             projbin = self.bin_ind(proj)
             pv[projbin] = pv[projbin] + 1
@@ -79,7 +85,7 @@ class MID:
         pv = pv/np.sum(pv)
         pvt = pvt/np.sum(pvt)
         safepv = np.copy(pv)
-        safepv[safepv==0] = 1 # prevents divide by zero errors when 0/0 below
+        safepv[safepv==0] = np.min(safepv[np.nonzero(safepv)]) # prevents divide by zero errors when 0/0 below
         info = 0 
         for ii in range(len(pvt)):
             info += (0 if pvt[ii] == 0 else pvt[ii]*np.log2(pvt[ii]/safepv[ii]))
@@ -101,25 +107,26 @@ class MID:
         svt = np.zeros_like(sv) # mean stim given projection and spike
         nstims = 0
         nspikes = 0
-        for stim, sp in self.analyzer.generator():
+        for stim, sp in self.handler.generator():
             proj = v.dot(stim)
             projbin = self.bin_ind(proj)
             pv[projbin] = pv[projbin] + 1
             pvt[projbin] = pvt[projbin] + sp
             sv[projbin] = sv[projbin] + stim
             svt[projbin] = svt[projbin] + sp*stim
-            nstims += 1
-            nspikes += sp       
         
-        pv = pv/np.sum(pv)
-        pvt = pvt/np.sum(pvt)
         
-        # to avoid dividing by zero I make zeros equal the next smallest value, which may cause problems if there are a lot of zeros
+        nstims = np.sum(pv)
+        nspikes = np.sum(pvt)
+        pv = pv/nstims
+        pvt = pvt/nspikes
+        
+        # to avoid dividing by zero I make zeros equal the next smallest possible value, which may cause problems if there are a lot of zeros
         safepv = np.copy(pv)
-        safepv[safepv==0] = np.min(safepv[np.nonzero(safepv)])
+        safepv[safepv==0] = 1./nstims
         sv = (sv/nstims)/safepv[:,np.newaxis]
-        safepvt = np.copy(pv)
-        safepvt[safepv==0] = np.min(safepvt[np.nonzero(safepvt)])
+        safepvt = np.copy(pvt)
+        safepvt[safepvt==0] = 1./nspikes
         svt = (svt/nspikes)/safepvt[:,np.newaxis]
         
         # Compute the derivative of the probability ratio wrt bin. 
@@ -151,8 +158,9 @@ class MID:
             infohist.append(info)
             gnorm = np.linalg.norm(grad)
             print(str(info)+'  '+str(gnorm))
-            v = v + rate*grad # should be plus, just messing around
+            v = v + rate*grad 
             it+=1
+        print(str(info)+'  '+str(gnorm))
         if gnorm<gtol:
             mes = "Converged to desired precision."
         else:
@@ -160,13 +168,13 @@ class MID:
         return SimpleResult(v, mes, history=infohist)
             
         
-    def optimize(self, method='BFGS', rate=1e-6):
+    def optimize(self, method='BFGS', rate=1e-6, maxiter=100):
         if method == 'BFGS':
-            result = minimize(self.info_grad,self.v,method=method, jac=True, options={'disp':True, 'maxiter':100})
+            result = minimize(self.info_grad,self.v,method=method, jac=True, options={'disp':True, 'maxiter':maxiter})
         elif method == 'Nelder-Mead':
             result = minimize(self.info, self.v, method=method, options={'disp':True})
         elif method == 'GA':
-            result = self.grad_ascent(self.v,rate)
+            result = self.grad_ascent(self.v,rate, maxiter=maxiter)
         else:
             return SimpleResult(self.v, "No valid method provided. Did nothing.")
         